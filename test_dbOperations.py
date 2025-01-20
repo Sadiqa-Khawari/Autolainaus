e91fe4e008869ffa686562f45566bbe382b893e0
Sadiqa-Khawari <53186@edu.raseko.fi>
2025-01-20 09:33:26 +0200

    Torstain 16.1 päätos

--- test_dbOperations.py
+++ test_dbOperations.py
@@ -10,9 +10,12 @@ settigsDectionary = {"server": "localhost",
                      "userName": "postgres",
                      "password": "Q2werty" }
 
+newValues = {"etunimi": "Ossian",
+             "sukunimi": "Onneton"}
+
 dbConnection = dbOperations.DbConnection(settigsDectionary)
 
-# TODO: Testaa, että yhteysmerkkijono muodostuu oikein
+# Testaa, että yhteysmerkkijono muodostuu oikein
 def test_connectionString():
     assert dbConnection.connectionString == f'dbname=testaus user=postgres password=Q2werty host=localhost port=5433'
 
@@ -21,7 +24,13 @@ def test_readOneRow():
     resultList = dbConnection.readAllColumnsFromTable("person") # Hakee taulun kaikki rivit listaan
     assert resultList[0] == (1, "Ville", "Virtanen") # Ensimmäinen rivi pitäisi olla 1 Ville Virtanen
 
-# TODO: Mieti mitä muita testejä pitää kirjoittaa
+# Testataan tietueen (record / row) lisäys tauluun (table)
+def test_addRow():
+    dbConnection.addToTable("person", newValues)
+    resultList = dbConnection.readAllColumnsFromTable("person")
+    rowCount = len(resultList)
+    assert resultList[rowCount-1] == (rowCount, "Ossian", "Onneton")
+
 
 
 
